472c6cc0882137ea29cf6a95c0615186a0501b62
Ankita karn <91003709+Karnankita04@users.noreply.github.com>
2024-11-21 19:13:19 +0530

    Update hangMan.js

--- hangMan.js
+++ hangMan.js
@@ -1,68 +1,60 @@
 function firstScene() {
-  console.log("┌──────────────────────────────────┐");
-  console.log("│   ┎---┑	                   │");
-  console.log("│   |                      (🥺)    │");
-  console.log("│   |                       |      │");
-  console.log("│   |                      / \\     │");
-  console.log("│   |______                        │");
-  console.log("└──────────────────────────────────┘");
+  console.log("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+  console.log("┃   ┏━━━┑	                   ┃");
+  console.log("┃   ┃                      (🥺)    ┃");
+  console.log("┃   ┃                       |      ┃");
+  console.log("┃   ┃                      / \\     ┃");
+  console.log("┃   ┗━━━━━━━                       ┃");
+  console.log("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
 }
 
 function secondScene() {
-  console.log("┌──────────────────────────────────┐");
-  console.log("│   ┎---┑	                   │");
-  console.log("│   |                 (😢)         │");
-  console.log("│   |                  |           │");
-  console.log("│   |                 / \\          │");
-  console.log("│   |______                        │");
-  console.log("└──────────────────────────────────┘");
+  console.log("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+  console.log("┃   ┏━━━┑	                   ┃");
+  console.log("┃   ┃                 (😢)         ┃");
+  console.log("┃   ┃                  |           ┃");
+  console.log("┃   ┃                 / \\          ┃");
+  console.log("┃   ┗━━━━━━                        ┃");
+  console.log("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
 }
 
 function thirdScene() {
-  console.log("┌──────────────────────────────────┐");
-  console.log("│   ┎---┑	                   │");
-  console.log("│   |            (😖)              │");
-  console.log("│   |             |                │");
-  console.log("│   |            / \\               │");
-  console.log("│   |______                        │");
-  console.log("└──────────────────────────────────┘");
+  console.log("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+  console.log("┃   ┏━━━┑	                   ┃");
+  console.log("┃   ┃            (😖)              ┃");
+  console.log("┃   ┃             |                ┃");
+  console.log("┃   ┃            / \\               ┃");
+  console.log("┃   ┗━━━━━━                        ┃");
+  console.log("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
 }
 
 function fourthScene() {
-  console.log("┌──────────────────────────────────┐");
-  console.log("│   ┎---┑	                   │");
-  console.log("│   |       (😭)                   │");
-  console.log("│   |        |                     │");
-  console.log("│   |       / \\                    │");
-  console.log("│   |______                        │");
-  console.log("└──────────────────────────────────┘");
+  console.log("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+  console.log("┃   ┏━━━┑	                   ┃");
+  console.log("┃   ┃       (😭)                   ┃");
+  console.log("┃   ┃        |                     ┃");
+  console.log("┃   ┃       / \\                    ┃");
+  console.log("┃   ┗━━━━━━                        ┃");
+  console.log("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
 }
 
-function fifthScene(answer) {
-  console.log("┌──────────────────────────────────┐");
-  console.log("│   ┎---┑	                   │");
-  console.log("│   |  (🫠)                        │");
-  console.log("│   |   |                          │");
-  console.log("│   |  / \\                         │");
-  console.log("│   |______                        │");
-  console.log("└──────────────────────────────────┘");
-
-  console.log("Committed Suicide ☠️ \n");
-  console.log("The answer is: ", answer);
+function fifthScene() {
+  console.log("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+  console.log("┃   ┏━━━┑	                   ┃");
+  console.log("┃   ┃  (🫠)                        ┃");
+  console.log("┃   ┃   |                          ┃");
+  console.log("┃   ┃  / \\                         ┃");
+  console.log("┃   ┗━━━━━━                        ┃");
+  console.log("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
 }
 
 function sceneToPlay(scene, answer) {
   switch (scene) {
-    case 1:
-      return firstScene();
-    case 2:
-      return secondScene();
-    case 3:
-      return thirdScene();
-    case 4:
-      return fourthScene();
-    case 5:
-      return fifthScene(answer);
+    case 1: return firstScene();
+    case 2: return secondScene();
+    case 3: return thirdScene();
+    case 4: return fourthScene();
+    case 5: return fifthScene(answer);
   }
 }
 
@@ -70,59 +62,51 @@ function won() {
   console.log("Congratulationsss🥳 It's correct");
 }
 
-function riddleQuestions(questionNum) {
-  switch (questionNum) {
-    case 1:
-      console.log("I am easy to lift, but hard to throw. What am I?");
-      return;
-    case 2:
-      console.log("what word contains 26 letters but only has three syllables");
-      return;
-    case 3:
-      console.log("I have many teeth but I can’t bite. What am I?");
-      return;
-    case 4:
-      console.log("What goes away as soon as you talk about it?");
-      return;
-    case 5:
-      console.log("I can be bigger than you but weigh nothing at all,What am I?");
-      return;
+function lost(answer) {
+  console.log("Committed Suicide ☠️ \n");
+  console.log("The answer is:", answer);
+}
+
+function riddleQuestions(question) {
+  switch (question) {
+    case 1: return "I am easy to lift, but hard to throw. What am I?";
+    case 2: return "what word contains 26 letters but only has three syllables";
+    case 3: return "I have many teeth but I can’t bite. What am I?";
+    case 4: return "What goes away as soon as you talk about it?";
+    case 5: return "I can be bigger than you but weigh nothing at all,What am I?";
   }
 }
 
-function riddleAnswers(answerNum) {
-  switch (answerNum) {
-    case 1:
-      return "feather";
-    case 2:
-      return "alphabet";
-    case 3:
-      return "comb";
-    case 4:
-      return "secret";
-    case 5:
-      return "shadow";
+function riddleAnswers(answer) {
+  switch (answer) {
+    case 1: return "feather";
+    case 2: return "alphabet";
+    case 3: return "comb";
+    case 4: return "secret";
+    case 5: return "shadow";
   }
 }
 
 function isGuessMatching(userGuess, questionNum) {
-  const actualAnswer = riddleAnswers(questionNum);
-  return actualAnswer === userGuess;
+  return riddleAnswers(questionNum) === userGuess;
 }
 
-function takeInput(numberOfIncorrect, questNum) {
+function takeInput(numberOfIncorrect, questionNum) {
   for (let chance = 5; chance >= 1; chance--) {
-    riddleQuestions(questNum);
+    console.log(riddleQuestions(questionNum));
     console.log("\nYou have", chance, "chances");
     const userGuess = prompt("guess😉: ").toLowerCase();
 
-    if (!isGuessMatching(userGuess, questNum)) {
-      console.clear();
-      numberOfIncorrect++;
-      sceneToPlay(numberOfIncorrect, riddleAnswers(questNum));
-    } else {
-      won();
-      break;
+    if (isGuessMatching(userGuess, questionNum)) {
+      return won();
+    }
+
+    console.clear();
+    numberOfIncorrect++;
+    sceneToPlay(numberOfIncorrect, riddleAnswers(questionNum));
+
+    if (chance === 1) {
+      return lost(riddleAnswers(questionNum));
     }
   }
 }
@@ -131,10 +115,10 @@ function starting() {
   console.log("WELCOME TO HANGMAN GAME \n");
   console.log("Guess the word related to given riddle\n");
 
-  let numberOfIncorrect = 0;
-  const questNum = Math.ceil(Math.random() * 5);
+  let noOfWrongGuess = 0;
+  const questionNum = Math.ceil(Math.random() * 5);
 
-  takeInput(numberOfIncorrect, questNum);
+  takeInput(noOfWrongGuess, questionNum);
 }
 
 starting();
