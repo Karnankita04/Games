9cc6f8553d6d740174d4ccbfba25ba66dd9f8d9e
Ankita karn <91003709+Karnankita04@users.noreply.github.com>
2024-11-23 18:47:07 +0530

    Update guessThesign.js

--- guessThesign.js
+++ guessThesign.js
@@ -1,3 +1,5 @@
+let userInput = "";
+
 function getFirstNumber() {
   return Math.round(Math.ceil(Math.random() * 10));
 }
@@ -24,20 +26,87 @@ function getSignNumber() {
   return signNumber;
 }
 
-function display() {
-  const number1 = getFirstNumber();
-  const number2 = getSecondNumber();
-  const signNumber = getSignNumber();
-  const result = getResult(signNumber, number1, number2);
-
+function display(number1, number2, result) {
   console.log(number1, "_", number2, "=", result);
 }
 
-function takeInput() {
-  prompt("Enter the sign: ");
+function isItAddition(number1, number2, result) {
+  return result - number2 === number1;
 }
 
-display();
-takeInput();
+function isItSubtraction(number1, number2, result) {
+  return result + number2 === number1;
+}
 
+function isItMultiplication(number1, number2, result) {
+  return result / number2 === number1;
+}
 
+function isItDivision(number1, number2, result) {
+  return result * number2 === number1;
+}
+
+function isEqual(firstSign, secondSign) {
+  return firstSign === secondSign;
+}
+
+function isValid(input) {
+  return (input === "+" || input === "-" || input === "*" || input === "/");
+}
+
+function isInputValid(number1, number2, result) {
+  if (isItAddition(number1, number2, result) && isEqual(userInput, "+")) {
+    userInput = "+";
+    return true;
+  }
+
+  if (isItSubtraction(number1, number2, result) && isEqual(userInput, "-")) {
+    userInput = "-";
+    return true;
+  }
+
+  if (isItMultiplication(number1, number2, result) && isEqual(userInput, "*")) {
+    userInput = "*";
+    return true;
+  }
+
+  if (isItDivision(number1, number2, result) && isEqual(userInput, "/")) {
+    userInput = "/";
+    return true;
+  }
+
+  if (!isValid(userInput)) {
+    return false;
+  }
+}
+
+function takeInput() {
+  return prompt("Enter the sign: ");
+}
+
+function getNumbers() {
+  while (true) {
+    const isInterested = confirm("Do you want to play?");
+
+    if (!isInterested) {
+      break;
+    }
+
+    const number1 = getFirstNumber();
+    const number2 = getSecondNumber();
+    const signNumber = getSignNumber();
+    const result = getResult(signNumber, number1, number2);
+
+    display(number1, number2, result);
+    userInput = takeInput();
+
+    if (!isInputValid(number1, number2, result)) {
+      console.log("INVALID INPUT");
+      continue;
+    }
+    
+    console.log(number1, userInput, number2, "=", result);
+  }
+}
+
+getNumbers();
